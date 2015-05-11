0da22fefc2b9b1eb765b9dcfa8ad177148fcb505
Logan Hasson <logan.hasson@gmail.com>
2015-05-10 21:35:58 -0400

    Decode JSON response

--- lib/issues/github_issues.ex
+++ lib/issues/github_issues.ex
@@ -6,10 +6,18 @@ defmodule Issues.GithubIssues do
     |> HTTPoison.get(@user_agent)
     |> handle_response
   end
+
   def issues_url(user, project) do
     "https://api.github.com/repos/#{user}/#{project}/issues"
   end
-  def handle_response({:ok, %HTTPoison.Response{body: body, headers: headers, status_code: 200}}), do: { :ok, body }
-  def handle_response({:ok, %HTTPoison.Response{body: body, headers: headers, status_code: ___}}), do: { :error, body }
-  def handle_response({:error, %HTTPoison.Response{body: body, headers: headers, status_code: ___}}), do: { :error, body }
+
+  def handle_response({:ok, %HTTPoison.Response{body: body, headers: headers, status_code: 200}}) do
+    { :ok, :jsx.decode(body) }
+  end
+  def handle_response({:ok, %HTTPoison.Response{body: body, headers: headers, status_code: ___}}) do
+    { :error, :jsx.decode(body) }
+  end
+  def handle_response({:error, %HTTPoison.Response{body: body, headers: headers, status_code: ___}}) do
+    { :error, :jsx.decode(body) }
+  end
 end
